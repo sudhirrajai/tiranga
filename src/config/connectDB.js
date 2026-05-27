8e3c3b0015a1b776e20a38858c18408c9a41e558
sudhirrajai <hackersmania125@gmail.com>
2026-05-27 23:58:02 +0530

    added nimbus yaml file

--- src/config/connectDB.js
+++ src/config/connectDB.js
@@ -1,10 +1,10 @@
 const mysql = require('mysql2/promise');
 
 const connection = mysql.createPool({
-    host: 'localhost',
-    user: 'tri',
-    password: 'tri',
-    database: 'tri',
+    host: process.env.DATABASE_HOST || 'localhost',
+    user: process.env.DATABASE_USER || 'tri',
+    password: process.env.DATABASE_PASSWORD || 'tri',
+    database: process.env.DATABASE_NAME || 'tri',
 });
 
 export default connection;
